cda412250622252d58a6703ef34894ee617ee16c
karthik.b <karthik.b@appviewx.com>
2018-11-16 18:26:19 +0530

    Random gen 2 methods

--- MoocFi/src/mooc/fi/PasswordRandomizer.java
+++ MoocFi/src/mooc/fi/PasswordRandomizer.java
@@ -7,19 +7,22 @@ public class PasswordRandomizer {
     private int passwordLength;
 
     public PasswordRandomizer(int passwordLength) {
-	random = new Random(passwordLength);
+	random = new Random(this.passwordLength);
 	this.passwordLength = passwordLength;
     }
 
     public String createPassword() {
 
+	StringBuilder bld = new StringBuilder();
 	char[] charArray = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
 	char[] result = new char[this.passwordLength];
 	for (int i = 0; i < this.passwordLength; i++) {
-	    result[i] = charArray[random.nextInt(this.passwordLength - 1)];
+	    // result[i] = charArray[random.nextInt(this.passwordLength) + 1];
+	    bld.append(charArray[random.nextInt(this.passwordLength) + 1]);
 	}
 
-	return new String(result);
+	// return new String(result);
+	return bld.toString();
     }
 
 }
--- MoocFi/src/mooc/fi/PasswordRandomizerMain.java
+++ MoocFi/src/mooc/fi/PasswordRandomizerMain.java
@@ -4,7 +4,7 @@ public class PasswordRandomizerMain {
 
     public static void main(String[] args) {
 
-	PasswordRandomizer randomizer = new PasswordRandomizer(20);
+	PasswordRandomizer randomizer = new PasswordRandomizer(40);
 	System.out.println("Password: " + randomizer.createPassword());
 	System.out.println("Password: " + randomizer.createPassword());
 	System.out.println("Password: " + randomizer.createPassword());
